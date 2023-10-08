4e889f05b1c6df27de028f1d3fce8f1a7d4166bc
Martin Ortega <lynxestudio@outlook.com>
2023-10-08 14:15:07 -0400

    Add source code to the project

---
+++ src/Puzzle2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Samples
+{
+public class Puzzle2
+{
+ public static int Main(string[] args)
+ {
+    Console.WriteLine(12345 + 5432l);
+    return 0;
+ }
+}
+}
---
+++ src/Puzzle3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Puzzle3
+{
+public static int Main(string[] args)
+{
+    char x = 'X';
+    int i = 0;
+    Console.Write(true ? x : 0);
+    Console.Write(false ? i : x);
+    Console.Write(Environment.NewLine);
+    return 0;
+}
+}
